42b6819de47a6d197f08fa59fcda01a57252835d
palenske <viniciuspalenske@gmail.com>
2022-04-08 00:08:22 -0300

    fixa dependencias em requirements.txt

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup
 
 setup(
     name="tech_news",
-    description="Projeto Tech News",
+    description="data scraping",
     install_requires=[
         "parsel==1.6.0",
         "requests==2.24.0",
         "pymongo==3.11.0",
-        "python-decouple==3.3",
+        "dnspython==2.2.1"
+        "python-decouple==3.3"
+        "cssselect==1.1.0",
+        "Flask==2.1.1",
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
